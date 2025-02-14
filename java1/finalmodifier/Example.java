cfa2d57fd6c4e3f4ed144db2002840f4bc1e09dd
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-02-14 14:05:52 +0100

    fix final demo

--- java1/finalmodifier/Example.java
+++ java1/finalmodifier/Example.java
@@ -7,7 +7,7 @@ public class Example {
     gender = 'w'; // nicht möglich keine neuzuweisung da final
 
     final Human steffen = new Human("Steffen");
-    steffen.lastName = "Stefinski"; // nicht möglich da const
+    steffen.firstName = "Stefinski"; // nicht möglich da const
     System.out.println(steffen.firstName);
     steffen.lastName = "Maglio"; // möglich!
     steffen = new Human("Marianna"); // nicht möglich keine neuzuweisung da final
