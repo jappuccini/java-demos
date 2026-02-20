4bf686985848d2bda674d7dc1be88953c9856759
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2026-02-20 13:14:43 +0100

    add multiple animals

--- java1/abstraction/Example.java
+++ java1/abstraction/Example.java
@@ -1,8 +1,21 @@
 package java1.abstraction;
 
+import java.util.ArrayList;
+
 public class Example {
   public static void main(String[] args) {
     // Animal animal = new Animal();
     // new Animal ist nicht möglich, da Animal eine abstrakte Klasse ist
+
+    ArrayList<Animal> animals = new ArrayList<>();
+    animals.add(new Cat());
+    animals.add(new Dog());
+    animals.add(new Cat());
+    animals.add(new Dog());
+    animals.add(new Cat());
+    animals.add(new Dog());
+    for (Animal animal : animals) {
+      animal.makeSound();
+    }
   }
 }
